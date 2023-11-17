abf907b4f57f4189553585c84d9b0ccf34f1952b
Rishika Pandit <102423889+Rishika-Pandit@users.noreply.github.com>
2023-11-17 17:08:58 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from pages.chat_with_csv import *
-from pages.chat_with_sql_db import *
+# from pages.chat_with_csv import *
+# from pages.chat_with_sql_db import *
 
 st.set_page_config(page_title="Home")
 st.title("This is the Home Page.") 
