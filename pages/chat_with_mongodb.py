ac33e672d3206956bc07820f75c8c67603237f75
Rishika Pandit <102423889+Rishika-Pandit@users.noreply.github.com>
2023-11-21 18:52:30 +0530

    Update chat_with_mongodb.py

--- pages/chat_with_mongodb.py
+++ pages/chat_with_mongodb.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+import ast
 import openai
 import json
 import streamlit as st
@@ -10,7 +10,7 @@ def textToMql(query,cursor):
     messages = [
     {
         "role": "system",
-        "content": f"You are a helpful assistant. The following is the document for reference from the given collection by the user {cursor}. Generate a MongoDB query to: {query} and give the filter in JSON format in one line. In the next line, provide the limit (if any specified by the user, else 0). For example:\n\nQ: 'Top 'n'(n is a number) restaurants based on filter condition given by user'\n\nA: {{\"column_name\": \"value\"}}\n n (value of n, i.e., limit). Note: Don't forget to add '\' in the provided JSON output if there is an apostrophe('). Also, make sure to give the JSON output in double quotes.\nA:"
+        "content": f"You are a helpful assistant. The following is the document for reference from the given collection by the user {cursor}. Generate a MongoDB query to: {query} and give the filter in JSON format in one line. If there is a projection, provide it in JSON format in the next line (if any specified by the user, else 'Null').In the subsequent line, provide the sorting condition (if any specified by the user) in a single new line. If a sorting condition is given, its contents should be given in a single new line. In the subsequent line, provide the limit (if any specified by the user, else 0). For example:\n\nQ: 'Names of top 'n'(n is a number) restaurants based on filter, projection, sort condition given by user'\n\nA: {{\"column_name\": \"value\"}}\n{{\"projection_name\": 1}}\n[(\"sort_name\", -1)]\n n (value of n, i.e., limit). Note: Don't forget to add '\' in the provided JSON output if there is an apostrophe('). Also, make sure to give the JSON output in double quotes.\nA:"
     }
 ],
     max_tokens=500,   
@@ -20,15 +20,15 @@ def textToMql(query,cursor):
 def clear_text():
     st.session_state["uri"] = ""
     st.session_state["query"] = ""
-    st.session_state["db_name"] = ""
-    st.session_state["collection"] = ""
+    # st.session_state["db_name"] = ""
+    # st.session_state["collection"] = ""
 
 def chat_with_mongodb_app():
     st.title("Chat With Your MongoDB Database!")
 
     uri = st.text_input("Enter the Link to Your MongoDB Database ", key="uri")
-    db_name = st.text_input("Enter the DB name of Your MongoDB Database ", key="db_name")
-    collection = st.text_input("Enter the Collection of Your MongoDB Database ", key="collection")
+    # db_name = st.text_input("Enter the DB name of Your MongoDB Database ", key="db_name")
+    # collection = st.text_input("Enter the Collection of Your MongoDB Database ", key="collection")
     OPENAI_API_KEY = st.text_input("Enter Your OpenAI Key ", key="key")
     openai.api_key =  OPENAI_API_KEY
     query = st.text_input("Enter Your Query", key="query")
@@ -44,26 +44,39 @@ def chat_with_mongodb_app():
         client = MongoClient(uri)
 
         # database and collection code goes here
-        db = client.db_name
-        coll = db.collection
+        db = client.sample_restaurants
+        coll = db.restaurants
         cursor = coll.find_one()
-        output =textToMql(query,cursor)
-        j_string, limit_str = output.split("\n")
+        output =textToMql(cursor, query)
+        j_filter, j_projection, j_list, limit_str = output.split("\n")
 
         # Convert limit to integer
         limit = int(limit_str)
-
-        # Check data type with type() method
-        j_string = j_string.replace("{$sort:", '{"$sort":')
+        if j_list != "Null":
+            j_sort = ast.literal_eval(j_list)
+        else: 
+            j_sort = None
         # Convert filter to JSON object
-        json_object = json.loads(j_string)
+        json_object_1 = json.loads(j_filter)
+        if j_projection != "Null":
+            json_object_2 = json.loads(j_projection)
 
-        if limit>0:
-            cursor_ans = coll.find(json_object).limit(limit)
+        if j_projection != 'None':
+            if j_sort != None:
+                cursor_ans = coll.find(json_object_1, json_object_2).sort(j_sort)
+                if limit>0:
+                    cursor_ans = coll.find(json_object_1, json_object_2).sort(j_sort).limit(limit)
+                else: 
+                    cursor_ans = coll.find(json_object_1, json_object_2).sort(j_sort) 
+            else:
+                if limit>0:
+                    cursor_ans = coll.find(json_object_1, json_object_2).limit(limit)
+                else: 
+                    cursor_ans = coll.find(json_object_1, json_object_2)
         else:
-            cursor_ans = coll.find(json_object)
+            cursor_ans = coll.find(json_object_1)
 
-        # answer_placeholder.text("Answer: ")
+        answer_placeholder.text("Answer: ")
         for doc in cursor_ans:
             answer_placeholder.text(doc)
 
